ae933f652b5547dd8a567aa9b58ef15b0e6e8d7d
Cameron Maske <cameronmaske@gmail.com>
2018-08-07 13:48:00 +0100

    Add dummy tests to test speed

--- tests/intergration_tests/example/test_api.py
+++ tests/intergration_tests/example/test_api.py
@@ -4,13 +4,6 @@ from example.models import Example
 
 
 @pytest.mark.django_db
-def test_list_empty(client):
-    response = client.get("/api/example/", format='json')
-    assert response.status_code == 200
-    assert response.json() == []
-
-
-@pytest.mark.django_db
 def test_create(client):
     response = client.post("/api/example/", data={'number': 2, 'text': "Hello World"}, format='json')
     assert response.status_code == 201
@@ -23,8 +16,9 @@ def test_create(client):
         'text': "Hello World"
     }
 
+# Simulate many tests
 
-@pytest.mark.django_db# Simulate many tests
+@pytest.mark.django_db
 def test_1(client):
     test_create(client)
 
@@ -62,4 +56,244 @@ def test_9(client):
 
 @pytest.mark.django_db
 def test_10(client):
+    test_create(client)
+
+@pytest.mark.django_db
+def test_11(client):
+    test_create(client)
+
+@pytest.mark.django_db
+def test_12(client):
+    test_create(client)
+
+@pytest.mark.django_db
+def test_13(client):
+    test_create(client)
+
+@pytest.mark.django_db
+def test_14(client):
+    test_create(client)
+
+@pytest.mark.django_db
+def test_15(client):
+    test_create(client)
+
+@pytest.mark.django_db
+def test_16(client):
+    test_create(client)
+
+@pytest.mark.django_db
+def test_17(client):
+    test_create(client)
+
+@pytest.mark.django_db
+def test_18(client):
+    test_create(client)
+
+@pytest.mark.django_db
+def test_19(client):
+    test_create(client)
+
+@pytest.mark.django_db
+def test_20(client):
+    test_create(client)
+
+@pytest.mark.django_db
+def test_21(client):
+    test_create(client)
+
+@pytest.mark.django_db
+def test_22(client):
+    test_create(client)
+
+@pytest.mark.django_db
+def test_23(client):
+    test_create(client)
+
+@pytest.mark.django_db
+def test_24(client):
+    test_create(client)
+
+@pytest.mark.django_db
+def test_25(client):
+    test_create(client)
+
+@pytest.mark.django_db
+def test_26(client):
+    test_create(client)
+
+@pytest.mark.django_db
+def test_27(client):
+    test_create(client)
+
+@pytest.mark.django_db
+def test_28(client):
+    test_create(client)
+
+@pytest.mark.django_db
+def test_29(client):
+    test_create(client)
+
+@pytest.mark.django_db
+def test_30(client):
+    test_create(client)
+
+@pytest.mark.django_db
+def test_31(client):
+    test_create(client)
+
+@pytest.mark.django_db
+def test_32(client):
+    test_create(client)
+
+@pytest.mark.django_db
+def test_33(client):
+    test_create(client)
+
+@pytest.mark.django_db
+def test_34(client):
+    test_create(client)
+
+@pytest.mark.django_db
+def test_35(client):
+    test_create(client)
+
+@pytest.mark.django_db
+def test_36(client):
+    test_create(client)
+
+@pytest.mark.django_db
+def test_37(client):
+    test_create(client)
+
+@pytest.mark.django_db
+def test_38(client):
+    test_create(client)
+
+@pytest.mark.django_db
+def test_39(client):
+    test_create(client)
+
+@pytest.mark.django_db
+def test_40(client):
+    test_create(client)
+
+@pytest.mark.django_db
+def test_41(client):
+    test_create(client)
+
+@pytest.mark.django_db
+def test_42(client):
+    test_create(client)
+
+@pytest.mark.django_db
+def test_43(client):
+    test_create(client)
+
+@pytest.mark.django_db
+def test_44(client):
+    test_create(client)
+
+@pytest.mark.django_db
+def test_45(client):
+    test_create(client)
+
+@pytest.mark.django_db
+def test_46(client):
+    test_create(client)
+
+@pytest.mark.django_db
+def test_47(client):
+    test_create(client)
+
+@pytest.mark.django_db
+def test_48(client):
+    test_create(client)
+
+@pytest.mark.django_db
+def test_49(client):
+    test_create(client)
+
+@pytest.mark.django_db
+def test_50(client):
+    test_create(client)
+
+@pytest.mark.django_db
+def test_51(client):
+    test_create(client)
+
+@pytest.mark.django_db
+def test_52(client):
+    test_create(client)
+
+@pytest.mark.django_db
+def test_53(client):
+    test_create(client)
+
+@pytest.mark.django_db
+def test_54(client):
+    test_create(client)
+
+@pytest.mark.django_db
+def test_55(client):
+    test_create(client)
+
+@pytest.mark.django_db
+def test_56(client):
+    test_create(client)
+
+@pytest.mark.django_db
+def test_57(client):
+    test_create(client)
+
+@pytest.mark.django_db
+def test_58(client):
+    test_create(client)
+
+@pytest.mark.django_db
+def test_59(client):
+    test_create(client)
+
+@pytest.mark.django_db
+def test_60(client):
+    test_create(client)
+
+@pytest.mark.django_db
+def test_61(client):
+    test_create(client)
+
+@pytest.mark.django_db
+def test_62(client):
+    test_create(client)
+
+@pytest.mark.django_db
+def test_63(client):
+    test_create(client)
+
+@pytest.mark.django_db
+def test_64(client):
+    test_create(client)
+
+@pytest.mark.django_db
+def test_65(client):
+    test_create(client)
+
+@pytest.mark.django_db
+def test_66(client):
+    test_create(client)
+
+@pytest.mark.django_db
+def test_67(client):
+    test_create(client)
+
+@pytest.mark.django_db
+def test_68(client):
+    test_create(client)
+
+@pytest.mark.django_db
+def test_69(client):
+    test_create(client)
+
+@pytest.mark.django_db
+def test_70(client):
     test_create(client)
---
+++ tests/unit_tests/test_examples.py
@@ -0,0 +1,220 @@
+def get_number():
+    for i in range(10000000):
+        yield i
+
+
+def expensive_function():
+    for n in get_number():
+        r = n ^ n ^ n
+    return f"some result! {r}"
+
+
+def test_1():
+    expensive_function()
+
+def test_2():
+    expensive_function()
+
+def test_3():
+    expensive_function()
+
+def test_4():
+    expensive_function()
+
+def test_5():
+    expensive_function()
+
+def test_6():
+    expensive_function()
+
+def test_7():
+    expensive_function()
+
+def test_8():
+    expensive_function()
+
+def test_9():
+    expensive_function()
+
+def test_10():
+    expensive_function()
+
+def test_11():
+    expensive_function()
+
+def test_12():
+    expensive_function()
+
+def test_13():
+    expensive_function()
+
+def test_14():
+    expensive_function()
+
+def test_15():
+    expensive_function()
+
+def test_16():
+    expensive_function()
+
+def test_17():
+    expensive_function()
+
+def test_18():
+    expensive_function()
+
+def test_19():
+    expensive_function()
+
+def test_20():
+    expensive_function()
+
+def test_21():
+    expensive_function()
+
+def test_22():
+    expensive_function()
+
+def test_23():
+    expensive_function()
+
+def test_24():
+    expensive_function()
+
+def test_25():
+    expensive_function()
+
+def test_26():
+    expensive_function()
+
+def test_27():
+    expensive_function()
+
+def test_28():
+    expensive_function()
+
+def test_29():
+    expensive_function()
+
+def test_30():
+    expensive_function()
+
+def test_31():
+    expensive_function()
+
+def test_32():
+    expensive_function()
+
+def test_33():
+    expensive_function()
+
+def test_34():
+    expensive_function()
+
+def test_35():
+    expensive_function()
+
+def test_36():
+    expensive_function()
+
+def test_37():
+    expensive_function()
+
+def test_38():
+    expensive_function()
+
+def test_39():
+    expensive_function()
+
+def test_40():
+    expensive_function()
+
+def test_41():
+    expensive_function()
+
+def test_42():
+    expensive_function()
+
+def test_43():
+    expensive_function()
+
+def test_44():
+    expensive_function()
+
+def test_45():
+    expensive_function()
+
+def test_46():
+    expensive_function()
+
+def test_47():
+    expensive_function()
+
+def test_48():
+    expensive_function()
+
+def test_49():
+    expensive_function()
+
+def test_50():
+    expensive_function()
+
+def test_51():
+    expensive_function()
+
+def test_52():
+    expensive_function()
+
+def test_53():
+    expensive_function()
+
+def test_54():
+    expensive_function()
+
+def test_55():
+    expensive_function()
+
+def test_56():
+    expensive_function()
+
+def test_57():
+    expensive_function()
+
+def test_58():
+    expensive_function()
+
+def test_59():
+    expensive_function()
+
+def test_60():
+    expensive_function()
+
+def test_61():
+    expensive_function()
+
+def test_62():
+    expensive_function()
+
+def test_63():
+    expensive_function()
+
+def test_64():
+    expensive_function()
+
+def test_65():
+    expensive_function()
+
+def test_66():
+    expensive_function()
+
+def test_67():
+    expensive_function()
+
+def test_68():
+    expensive_function()
+
+def test_69():
+    expensive_function()
+
+def test_70():
+    expensive_function()
